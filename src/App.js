653903e64dd8cee7288983c2e18e294682cab19c
Mateus Castellar <mateuscastellar@outlook.com>
2022-12-07 09:43:08 -0300

    feat: Navbar e footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar/>
         <div className="container">
           <Routes>
             <Route path="/" element={<Home/>}/>
@@ -14,6 +17,7 @@ function App() {
             <Route path="/about" element={<About/>}/>
           </Routes>
         </div>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
