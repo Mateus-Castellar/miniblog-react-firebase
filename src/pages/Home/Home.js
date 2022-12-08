8a0a5c7e77d1c35480c5c207fe65d361ac6044b0
Mateus Castellar <mateuscastellar@outlook.com>
2022-12-08 16:31:51 -0300

    feat: Listagem de posts na HomePage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,42 @@
 import styles from "./Home.module.css";
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+import {useFetchDocuments} from "../../hooks/useFetchDocuments"
+import PostDetail from "../../components/PostDetail";
 
 const Home = () => {
+
+  const [query, setQuery] = useState("");
+  const {documents:posts, loading, error} = useFetchDocuments("posts"); 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={styles.home}>
-      <h3>Home Page - Postagens</h3>
+      <h1>Veja nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="busque por tags.." />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+
+      <div>
+
+        {loading && <p>Carregando Posts...</p>}
+
+        {posts && posts.map((post) => (
+          <PostDetail key={post.id} post={post}/>
+        ))}
+
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="posts/create" className="btn">Criar Primeiro Post</Link>
+          </div>
+        )}
+      </div>
+
     </div>
   )
 }
